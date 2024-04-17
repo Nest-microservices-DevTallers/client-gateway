659ca34a7c0a97f187e9928afca8b42861593b5c
BetoNajera <ing.betonajera@gmail.com>
2024-04-17 16:03:52 -0600

    :sparkles: Feat: Add connection tcp with microservice
    Add connection tcp with microservice

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,7 +1,23 @@
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { Module } from '@nestjs/common';
+
+import { envs, PRODUCT_SERVICE } from '@server/config';
+
 import { ProductsController } from './products.controller';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: PRODUCT_SERVICE,
+        transport: Transport.TCP,
+        options: {
+          host: envs.productsMicroserviceHost,
+          port: envs.productsMicroservicePort,
+        },
+      },
+    ]),
+  ],
   controllers: [ProductsController],
 })
-export class ProductsModule { }
+export class ProductsModule {}
