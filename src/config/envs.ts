43f2f4851fb38b0938e45fb0b2ed962ef8e92d73
BetoNajera <ing.betonajera@gmail.com>
2024-04-21 18:39:24 -0600

    :sparkles: Feat: Create envs varibles to nats
    Create envs varibles to nats

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,25 +3,18 @@ import 'dotenv/config';
 
 interface EnvVars {
   PORT: number;
-
-  PRODUCTS_MICROSERVICE_HOST: string;
-  PRODUCTS_MICROSERVICE_PORT: number;
-
-  ORDERS_MICROSERVICE_HOST: string;
-  ORDERS_MICROSERVICE_PORT: number;
+  NATS_SERVERS: string[];
 }
 
 const envSchema = Joi.object({
   PORT: Joi.number().required(),
-
-  PRODUCTS_MICROSERVICE_HOST: Joi.string().required(),
-  PRODUCTS_MICROSERVICE_PORT: Joi.number().required(),
-
-  ORDERS_MICROSERVICE_HOST: Joi.string().required(),
-  ORDERS_MICROSERVICE_PORT: Joi.number().required(),
+  NATS_SERVERS: Joi.array().items(Joi.string()).required(),
 }).unknown(true);
 
-const { error, value } = envSchema.validate(process.env);
+const { error, value } = envSchema.validate({
+  ...process.env,
+  NATS_SERVERS: process.env.NATS_SERVERS?.split(','),
+});
 
 if (error) throw new Error(`Config calidation error: ${error.message}`);
 
@@ -30,9 +23,5 @@ const envVars: EnvVars = value;
 export const envs = {
   port: envVars.PORT,
 
-  productsMicroserviceHost: envVars.PRODUCTS_MICROSERVICE_HOST,
-  productsMicroservicePort: envVars.PRODUCTS_MICROSERVICE_PORT,
-
-  ordersMicroserviceHost: envVars.ORDERS_MICROSERVICE_HOST,
-  ordersMicroservicePort: envVars.ORDERS_MICROSERVICE_PORT,
+  natsServers: envVars.NATS_SERVERS,
 };
